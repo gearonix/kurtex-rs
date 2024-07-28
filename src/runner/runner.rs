2598d10425db6a4eb2652b448b4f461e68ec16ca
Uzhanin Egor <egor.uzhanin@gmail.com>
2024-07-29 01:06:20 +0300

    fix: bug with excluded cases, rewrite to glob

--- src/runner/runner.rs
+++ src/runner/runner.rs
@@ -1,7 +1,9 @@
 use deno_core::error::AnyError;
+use globmatch::Matcher;
 use globwalk;
 use globwalk::DirEntry;
 use regex::Regex;
+use std::borrow::Cow;
 
 use crate::config::{get_or_init_cli_config, get_or_init_runtime_cfg};
 use crate::runtime::runtime::RuntimeConfig;
@@ -15,7 +17,7 @@ impl Runner {
     let cli_config = get_or_init_cli_config(None);
     let runtime_config = get_or_init_runtime_cfg(None);
 
-    let RuntimeConfig { options: runtime_opts, root, .. } = runtime_config;
+    // let RuntimeConfig { options: runtime_opts, root, .. } = runtime_config;
 
     if cli_config.watch {
       // TODO
@@ -31,37 +33,25 @@ impl Runner {
 
   fn collect_test_files(
     runtime_cfg: &RuntimeConfig,
-  ) -> Result<impl Iterator<Item = DirEntry> + '_, AnyError> {
+  ) -> Result<impl Iterator<Item = std::path::PathBuf> + '_, AnyError> {
     let runtime_opts = &runtime_cfg.options;
+    let walk_glob = |patterns: &Vec<Cow<'static, str>>| {
+      globwalk::GlobWalkerBuilder::from_patterns(&runtime_cfg.root, patterns)
+        .max_depth(TEST_FILES_MAX_DEPTH as usize)
+        .build()
+        .unwrap()
+        .into_iter()
+        .filter_map(Result::ok)
+        .map(|entry| entry.into_path())
+    };
 
-    let walker = globwalk::GlobWalkerBuilder::from_patterns(
-      &runtime_cfg.root,
-      &runtime_opts.includes,
-    )
-    .max_depth(TEST_FILES_MAX_DEPTH as usize)
-    .build()?
-    .into_iter()
-    .filter_map(Result::ok);
+    #[allow(unused_mut)]
+    let mut included_cases = walk_glob(&runtime_opts.includes);
+    let mut excluded_cases = walk_glob(&runtime_opts.excludes);
 
-    let test_files = walker.filter(|dir_entry| {
-      let walker_path = dir_entry.path().to_string_lossy();
-
-      let is_excluded = runtime_opts.excludes.iter().any(|excluded_path| {
-        let excluded = runtime_cfg.root.join(excluded_path.as_ref());
-        let exclude_re =
-          Regex::new(excluded.to_string_lossy().as_ref()).unwrap();
-
-        
-        println!("exclude_re: {:?}", exclude_re);
-        println!("excluded: {:?}", excluded);
-        
-        exclude_re.is_match(walker_path.as_ref())
-      });
-
-      !is_excluded
-    });
-
-    Ok(test_files)
+    Ok(included_cases.filter(move |included_path| {
+      !excluded_cases.any(|excluded_path| excluded_path.eq(included_path))
+    }))
   }
 
   pub fn run() {}
