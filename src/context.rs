df1366fd9f909fc93df5e9d250606e6a3a5de534
Uzhanin Egor <egor.uzhanin@gmail.com>
2024-08-03 02:25:48 +0300

    feat: remove unused lazy_static context variable

--- src/context.rs
+++ src/context.rs
@@ -1,19 +1,13 @@
-use std::cell::{OnceCell, RefCell};
 use std::sync::OnceLock;
 
 use tokio::runtime::Runtime;
 
-use crate::runner::context::CollectorContext;
 use crate::runtime::runtime::RuntimeConfig;
 use crate::CliConfig;
 
 pub static TOKIO_RUNTIME: OnceLock<Runtime> = OnceLock::new();
 pub static CLI_CONFIG: OnceLock<CliConfig> = OnceLock::new();
 pub static RUNTIME_CONFIG: OnceLock<RuntimeConfig> = OnceLock::new();
-
-thread_local! {
-    pub static COLLECTOR_CONTEXT: RefCell<CollectorContext> = RefCell::new(CollectorContext::new());
-}
 
 pub struct ContextProvider;
 
