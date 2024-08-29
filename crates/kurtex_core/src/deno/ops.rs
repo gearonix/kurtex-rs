ff4ae879c5d20a2e503742c4e2b9b41d8817f826
Uzhanin Egor <egor.uzhanin@gmail.com>
2024-08-29 12:55:26 +0300

    fix(ops): add method parameter to deno_core op2 macro

--- crates/kurtex_core/src/deno/ops.rs
+++ crates/kurtex_core/src/deno/ops.rs
@@ -7,19 +7,23 @@ use crate::collector::{
 };
 use crate::deno::ExtensionLoader;
 use crate::{CollectorMetadata, TestCallback};
+use deno_core::op2 as op;
 
 // Kurtex V8 -> Rust interface layer.
 pub struct CollectorRegistryExt;
+
+impl deno_core::GarbageCollected for CollectorRegistryExt {}
 
 impl CollectorRegistryExt {
   pub fn new() -> Self {
     CollectorRegistryExt
   }
 
-  #[deno_core::op2]
+  #[op(method(CollectorRegistryExt))]
   #[meta(sanitizer_details = "register new task unit")]
   #[meta(sanitizer_fix = "awaiting identifier and callback")]
   fn op_register_collector_task(
+    &self,
     #[state] collector_ctx: &CollectorContext,
     #[string] identifier: String,
     #[from_v8] callback: TestCallback,
@@ -31,10 +35,11 @@ impl CollectorRegistryExt {
       .register_task(identifier, callback, run_mode)
   }
 
-  #[deno_core::op2]
+  #[op(method(CollectorRegistryExt))]
   #[meta(sanitizer_details = "register new test node (suite)")]
   #[meta(sanitizer_fix = "awaiting identifier and callback")]
   fn op_register_collector_node(
+    &self,
     #[state] collector_ctx: &mut CollectorContext,
     #[from_v8] identifier: CollectorIdentifier,
     #[from_v8] factory: TestCallback,
@@ -47,10 +52,11 @@ impl CollectorRegistryExt {
     ));
   }
 
-  #[deno_core::op2]
+  #[op(method(CollectorRegistryExt))]
   #[meta(sanitizer_details = "register new test lifetime hook")]
   #[meta(sanitizer_fix = "awaiting lifetime hook type and callback")]
   fn op_register_lifetime_hook(
+    &self,
     #[state] collector_ctx: &CollectorContext,
     #[from_v8] lifetime_hook: LifetimeHook,
     #[from_v8] callback: TestCallback,
